e116a0d32ae48d1ee672b5d3dab25374b52aa797
julia-heo <100216331+julia-heo@users.noreply.github.com>
2023-12-26 09:56:13 +0900

    [feat] settings update

--- MyMap/settings/base.py
+++ MyMap/settings/base.py
@@ -102,7 +102,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'allauth.account.middleware.AccountMiddleware', # 추가
+    # 'allauth.account.middleware.AccountMiddleware', # 추가
     'corsheaders.middleware.CorsMiddleware'
 ]
 
